491dc1712ce32b08ba3ff8c90088c6644fd2a7ed
igor martins <igorm409@gmail.com>
2022-05-07 20:38:12 -0300

    Mapeando as Classes Com JPA

--- bankline-api/src/main/java/com/dio/santander/banklineapi/model/Movimentacao.java
+++ bankline-api/src/main/java/com/dio/santander/banklineapi/model/Movimentacao.java
@@ -1,5 +1,59 @@
 package com.dio.santander.banklineapi.model;
 
-public class Movimentacao {
+import java.time.LocalDateTime;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tab_movimentacao")
+public class Movimentacao {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY )
+	private Integer id;
+	private LocalDateTime dataHora;
+	private String descricao;
+	private Double valor;
+	
+	@Enumerated(EnumType.STRING)
+	private TipoMovimentacao tipo;
+	public int getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	@Column(name = "data_hora")
+	public LocalDateTime getDataHora() {
+		return dataHora;
+	}
+	public void setDataHora(LocalDateTime dataHora) {
+		this.dataHora = dataHora;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public Double getValor() {
+		return valor;
+	}
+	public void setValor(Double valor) {
+		this.valor = valor;
+	}
+	public TipoMovimentacao getTipo() {
+		return tipo;
+	}
+	public void setTipo(TipoMovimentacao tipo) {
+		this.tipo = tipo;
+	}
+	
 }
